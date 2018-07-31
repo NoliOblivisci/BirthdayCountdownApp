2ab25de105fd172c89e629e11e1ca4d29276b142
Miles Dei <amicusiesu@gmail.com>
2018-07-31 14:26:19 +0000

    styled picker

--- src/components/picker.js
+++ src/components/picker.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 
+import 'react-datepicker/dist/react-datepicker.css';
+
 class Picker extends Component {
     
     constructor(props) {
         super(props);
         this.state = {
-            start.Date: moment()
+            startDate: moment()
         };
         this.handleChange = this.handleChange.bind(this);
     }
